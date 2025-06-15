1c8a65a521576402d7e77162e9acbf3b7722cbbd
Victoria Ortiz <ortiz.victoria@ufl.edu>
2025-06-15 10:51:42 -0400

    finally added todo list functionality with add and remove features after lots of trial & error sessions

--- index.js
+++ index.js
@@ -2,3 +2,27 @@ var form = document.getElementById('add-todo');
 var list = document.getElementById('todo-list');
 var todoInput = document.querySelector('input');
 var todos = [];
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+var newTodoTask = todoInput.value.trim();
+    if (newTodoTask.length > 0) {
+        addNewTodo(newTodoTask);
+         todoInput.value = '';
+}
+});
+function addNewTodo(taskInput) {
+var newTodoEntry = document.createElement('li');
+var addTodoButton = document.createElement('button');
+addTodoButton.textContent = taskInput;
+
+addTodoButton.addEventListener('click', function() {
+    if (addTodoButton.style.textDecoration === 'line-through') {
+    list.removeChild(newTodoEntry);
+}
+    else {
+    addTodoButton.style.textDecoration = 'line-through';
+    }
+});
+newTodoEntry.appendChild(addTodoButton);
+list.appendChild(newTodoEntry);
+}
